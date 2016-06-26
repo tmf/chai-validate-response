624af0879a5be859abe7c24c994b22f1bbea76b2
tmf <tom.forrer@gmail.com>
2016-06-26 23:45:39 +0200

    feat: support superagent responses
    - they have a slightly different interface than whatwg-fetch’s
    interface…
    - use the invertWhenNegated in assertions that should be true, like the
    endpoint should at least produce the content-type of the response and
    possibly have a response defined in the schema for the returned status
    code especially while the assertion chain is negated: if the test
    expects the response to be invalid according to schema, path and
    status, the response content-type and status should at least be
    allowed/defined by the schema.

--- test/chai-validate-response.spec.js
+++ test/chai-validate-response.spec.js
@@ -5,6 +5,8 @@ import chaiValidateResponse from "../lib/chai-validate-response";
 import chaiAsPromised from "chai-as-promised";
 import generateSchema from "./helper/generate-schema";
 import generateJsonResponse from "./helper/generate-json-response";
+import express from "express";
+import supertest from "supertest";
 
 chai.use(chaiValidateResponse);
 
@@ -31,6 +33,15 @@ describe("chai-validate-response", () => {
 
             expect(response).not.to.be.a.validResponse(schema, "/", "get").andNotifyWhen(done);
         });
+
+        it("should work with express / superagent", (done) => {
+            let app = express();
+            app.get("/", (req, res) => res.status(200).json({ foo: true }));
+
+            supertest(app)
+                .get("/")
+                .expect(200)
+                .then((res) => expect(res).to.be.a.validResponse(schema, "/", "get").andNotifyWhen(done));
         });
 
         it("should work with the chai-as-promised plugin", (done) => {
